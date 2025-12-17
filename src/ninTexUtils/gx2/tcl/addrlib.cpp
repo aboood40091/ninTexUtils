4b4dc59ebd8f05d8f57bd0a930bf4fe4cfb5577e
AboodXD <aboood40091@gmail.com>
2025-12-18 02:04:39 +0400

    addrlib: Remove unused functions

--- src/ninTexUtils/gx2/tcl/addrlib.cpp
+++ src/ninTexUtils/gx2/tcl/addrlib.cpp
@@ -8,17 +8,7 @@ static inline bool IsPow2(u32 dim)
     return !(dim & (dim - 1));
 }
 
-static inline bool IsPow2(u64 dim)
-{
-    return !(dim & (dim - 1));
-}
-
 static inline u32 PowTwoAlign(u32 x, u32 align)
-{
-    return (x + (align - 1)) & (~(align - 1));
-}
-
-static inline u64 PowTwoAlign(u64 x, u64 align)
 {
     return (x + (align - 1)) & (~(align - 1));
 }
